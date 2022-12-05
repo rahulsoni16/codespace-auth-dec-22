498fc48f31aaa26fadbbf948b4f62cf80334904f
Rahul Soni <rahul.soni16@gmail.com>
2022-12-05 15:51:10 +0000

    index.js: not found

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./app')
 const { PORT } = process.env
 app.listen(PORT, () => {
-    console.log('Server is running at port: ${PORT}');
+    console.log(`Server is running at port: ${PORT} `);
 })
